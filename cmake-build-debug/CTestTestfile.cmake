0773e6e58c54e9d627f61d54fcad6657bf2a0ea5
Jose Artur <joseartura1.2@gmail.com>
2024-05-07 18:13:27 +0100

    removed cmake-build

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/ctw02488/Documents/Tests/NitroCppTest-JoseArtur
-# Build directory: /mnt/c/Users/ctw02488/Documents/Tests/NitroCppTest-JoseArtur/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
